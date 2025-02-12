6c733d8c469fd47681a11e0510a41c2d58d2c43b
xavier-miles <xmiles99@gmail.com>
2025-02-12 11:52:32 -0500

    Added components for each required section of the webpage. Also set up routing to each page.

---
+++ maxx-energy-portal/src/app/components/f-a-q/f-a-q.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-f-a-q',
+  imports: [],
+  templateUrl: './f-a-q.component.html',
+  styleUrl: './f-a-q.component.scss'
+})
+export class FAQComponent {
+
+}
