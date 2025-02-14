e5d9950cfafaab9e7a79780fc05d3095c2ecf9f9
xavier-miles <xmiles99@gmail.com>
2025-02-14 10:13:59 -0500

    Made empty directory redirect to /home. Added basic Hero image on home-component.

--- maxx-energy-portal/src/app/app.routes.ts
+++ maxx-energy-portal/src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'about-us', component: AboutUsComponent},
   {path: 'contact-us', component: ContactUsComponent},
