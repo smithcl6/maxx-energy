94836b5a1ab42eafb5f93f186e1cdda8aafbae10
xavier-miles <xmiles99@gmail.com>
2025-02-21 11:29:10 -0500

    Added "no results found" to FAQ search, added login/logout function to nav-bar and tidied code.

--- maxx-energy-portal/src/app/components/nav-bar/nav-bar.component.ts
+++ maxx-energy-portal/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,19 @@
+import { ApiService } from './../../services/api.service';
 import { RouterModule, RouterLinkActive } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { NgIf, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterModule, RouterLinkActive],
+  imports: [RouterModule, RouterLinkActive, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+AuthenticationService = inject(AuthenticationService);
+ApiService = inject(ApiService);
+
+constructor() {}
 
 }
