efe939ffe45301c96dd0c9aee856b0e1b0c5858b
Christopher Smith <smithcl6@vcu.edu>
2025-02-24 19:37:04 -0500

    Login page now has a form that takes email and password.

--- maxx-energy-portal/src/app/components/login/login.component.ts
+++ maxx-energy-portal/src/app/components/login/login.component.ts
@@ -2,27 +2,53 @@ import { AuthenticationService } from './../../services/authentication.service';
 import { IUser } from '../../models/IUser';
 import { ApiService } from './../../services/api.service';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
+/**
+ * User Login Page.
+ */
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   private ApiService: ApiService = inject(ApiService);
+  private readonly router: Router = inject(Router);
   protected AuthenticationService: AuthenticationService = inject(AuthenticationService);
+  protected userForm: FormGroup<LoginForm> = new FormGroup<LoginForm>({
+    email: new FormControl<string>('', {nonNullable: true, validators: Validators.email}),
+    password: new FormControl<string>('', {nonNullable: true})
+  });
+  protected loginError: string = '';
 
-  login() {
-    const user: IUser = {
-      email: 'hi@email.com',
-      name: 'Chris',
-      password: 'abc123'
+  /**
+   * Send login form data to API service.
+   */
+  protected async submitLogin() {
+    try {
+      const user: IUser = {
+        email: this.userForm.getRawValue().email,
+        name: '',
+        password: this.userForm.getRawValue().password
+      }
+      await this.ApiService.login(user);
+      this.userForm.reset();
+      this.loginError = '';
+      // this.router.navigate(['/home']);
+
+    } catch (error) {
+      this.loginError = 'Invalid email/password';
     }
-    this.ApiService.login(user);
   }
+}
 
-  logout() {
-    this.ApiService.logout();
-  }
+/**
+ * Defines what will be required in the Login Form.
+ */
+interface LoginForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
 }
