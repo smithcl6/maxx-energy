55ee2f48a96837e8ebe2246872cdcf143d150e19
xavier-miles <xmiles99@gmail.com>
2025-03-03 12:32:01 -0500

    Profile logic is complete.

--- maxx-energy-portal/src/app/components/profile/profile.component.ts
+++ maxx-energy-portal/src/app/components/profile/profile.component.ts
@@ -3,44 +3,55 @@ import { AuthenticationService } from './../../services/authentication.service';
 import { Component, inject } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { IUser } from '../../models/IUser';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 1
 @Component({
   selector: 'app-profile',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   protected AuthenticationService: AuthenticationService = inject(AuthenticationService);
 
+  private ApiService: ApiService = inject(ApiService);
+
   constructor(public router: Router) { }
 
   isEditEnabled: boolean = false;
-/**
- * Enables or disables edit mode for user profile.
- */
+
+  NameInputForm = new FormGroup({
+    name: new FormControl(this.AuthenticationService.getUserDetails()?.name)
+  });
+
+  EmailInputForm = new FormGroup({
+    email: new FormControl(this.AuthenticationService.getUserDetails()?.email)
+  })
+
+  /**
+   * Enables or disables edit mode for user profile.
+   */
   editMode() {
     this.isEditEnabled = !this.isEditEnabled;
     console.log('Edit mode status changed to ' + this.isEditEnabled);
   }
 
-/**
- * Submits changes to user profile to backend for updating.
- */
-  submitChanges() {
-    //TODO: add logic to submit changes to backend
-    console.log('Changes submitted');
+  // Using this for testing API service and backend
+  /**
+   * Submits user profile changes to backend for updating.
+   */
+  protected submitProfileForm() {
+    const user: IUser = {
+      email: this.EmailInputForm.get('email')?.value || '',
+      name: this.NameInputForm.get('name')?.value || '',
+    };
+    this.ApiService.updateProfile(user);
+    console.log('Changes submitted. Name: ' + this.AuthenticationService.getUserDetails()?.name + ', Email: ' + this.AuthenticationService.getUserDetails()?.email);
     this.editMode();
   }
 
-  private ApiService: ApiService = inject(ApiService);
-
-  // Using this for testing API service and backend
-  protected submitProfileForm() {
-    const user: IUser = {
-      email: 'updated@email.com',
-      name: 'Updated Name'
-    };
-    this.ApiService.updateProfile(user);
+// For Debug
+  DisplayName(){
+    console.log(this.AuthenticationService.getUserDetails()?.name + ", " + this.AuthenticationService.getUserDetails()?.email);
   }
 }
