486a361b2be2944179c37b071b8d0312fc7c5ce3
KGprime13 <kgandrews13@gmail.com>
2025-03-05 10:36:09 -0500

    added more TS for API

--- maxx-energy-portal/src/app/components/contact-us/contact-us.component.ts
+++ maxx-energy-portal/src/app/components/contact-us/contact-us.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup,ReactiveFormsModule } from '@angular/forms';
-
+import { ApiService } from '../../services/api.service';
 @Component({
   selector: 'app-contact-us',
-  imports: [ReactiveFormsModule,],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
@@ -13,7 +13,22 @@ export class ContactUsComponent {
     email:new FormControl(''),
     message:new FormControl('')
   });
-  onSubmit(){
-    console.log(this.contactForm.value)  //would assume backend gets linked here somewhere.
+//api call
+  async onSubmit(){
+    console.log(this.contactForm.value)
+    try {
+      const contactObject = {
+        name: this.contactForm.getRawValue().name,
+        email: this.contactForm.getRawValue().email,
+        message: this.contactForm.getRawValue().message
+      }
+      await this.ApiService.contactCompany(contactObject);
+    } catch (error) {
+      console.error(error);
+      // Show some sort of error message take place
+      alert('Something went wrong while submitting your message. Please try again.');
+    }
   }
+
+
 }
