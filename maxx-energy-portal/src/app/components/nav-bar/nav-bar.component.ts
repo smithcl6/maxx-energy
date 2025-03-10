cd7d2868754fd65f5d3c12c851f9eddceed4313a
xavier-miles <xmiles99@gmail.com>
2025-03-10 12:15:39 -0400

    Made navbar collapse on scroll and will be sticky.

--- maxx-energy-portal/src/app/components/nav-bar/nav-bar.component.ts
+++ maxx-energy-portal/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { ApiService } from './../../services/api.service';
 import { RouterModule, RouterLinkActive } from '@angular/router';
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { NgIf, CommonModule } from '@angular/common';
 
@@ -11,9 +11,27 @@ import { NgIf, CommonModule } from '@angular/common';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
-AuthenticationService = inject(AuthenticationService);
-ApiService = inject(ApiService);
+  AuthenticationService = inject(AuthenticationService);
+  ApiService = inject(ApiService);
 
-constructor() {}
+  constructor() {}
+  @ViewChild('navbar') navbar!: ElementRef;
+  isCollapsed = false;
 
+  // Track initial navbar position
+  navbarPosition: number = 100;
+
+  ngAfterViewInit() {
+    this.navbarPosition = this.navbar.nativeElement.offsetTop;
+  }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    const windowScroll = window.pageYOffset;
+
+    if (windowScroll > this.navbarPosition) {
+      this.isCollapsed = true;
+    } else { 
+      this.isCollapsed = false;
+    }
+  }
 }
